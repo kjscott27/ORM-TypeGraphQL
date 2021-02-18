cb8530c73cd3e68e4d4f9359d6baafae1620da55
Kyle Scott <kyle.j.scott@hotmail.com>
2021-02-17 22:33:18 -0500

    add redis and express-session

--- src/globals/types.ts
+++ src/globals/types.ts
@@ -1,5 +1,8 @@
 import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
+import { Request, Response } from 'express';
 
 export type ContextType = {
   em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+  req: Request,
+  resposne: Response
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import 'reflect-metadata'
+import redis from 'redis';
+import session from 'express-session';
+import connectRedis from 'connect-redis';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import express from 'express';
@@ -6,6 +9,7 @@ import mikroConfig from './mikro-orm.config';
 import { MikroORM } from '@mikro-orm/core';
 import { PostResolver } from './resolvers/posts';
 import { UserResolver } from './resolvers/user';
+import { runningInProduction } from './globals/constants';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -13,12 +17,32 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  // the session middleware needs to run before the apollo middleware
+  app.use(
+    session({
+      name: 'qid',
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        //
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: 'lax', // CSRF protection
+        secure: runningInProduction // cookie only on https
+      },
+      secret: 'keyboard cat',
+      resave: false,
+    })
+  )
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PostResolver, UserResolver],
       validate: false
     }),
-    context: () => ({ em: orm.em })
+    context: ({ req, res }) => ({ em: orm.em, req, res })
   })
 
   apolloServer.applyMiddleware({ app });
